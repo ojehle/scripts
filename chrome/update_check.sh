21583e42588e6421337a31704d0a11a411b6d2b5
Oliver Jehle <ojehle@yahoo.com>
2022-02-09 11:25:36 +0100

    fix update of log file

--- chrome/update_check.sh
+++ chrome/update_check.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+date >/tmp/google_softwareupdate.log
 rm -f /Users/oj/Library/Google/GoogleSoftwareUpdate/Stats/Keystone.stats 2>/dev/null
-/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Resources/GoogleSoftwareUpdateAgent.app/Contents/MacOS/GoogleSoftwareUpdateAgent -runMode oneshot -userInitiated YES ”$@”
+/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Resources/GoogleSoftwareUpdateAgent.app/Contents/MacOS/GoogleSoftwareUpdateAgent -runMode oneshot -userInitiated YES ”$@” 2>&1 >>/tmp/google_softwareupdate.log
