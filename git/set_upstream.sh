241dcab2cfac9e18fa1138c146843347e7914dbb
Oliver Jehle <ojehle@yahoo.com>
2022-10-10 09:29:34 +0200

    add automatic

--- git/set_upstream.sh
+++ git/set_upstream.sh
@@ -1,1 +1,7 @@
-echo git branch --set-upstream-to myfork/master
+#echo git branch --set-upstream-to myfork/master
+if git branch -v | grep -q main; then
+  git branch --set-upstream-to origin/main
+else
+  echo git branch --set-upstream-to origin/XXXX
+fi
+  
