be8d25b045a48809d86a1ed8882953b70f9f37be
Oliver Jehle <ojehle@yahoo.com>
2022-10-10 09:08:49 +0200

    add all repo

--- git/add_remote.sh
+++ git/add_remote.sh
@@ -21,3 +21,12 @@ else
   git remote add github git@github.com:ojehle/$repo
   echo github nok
 fi
+
+if git remote -v |  grep  all; then
+  echo all ok
+else
+  git remote add all dream920:/volume1/gitserver/oj/$repo
+  git remote set-url --add all git@github.com:ojehle/$repo
+  git remote set-url --add all git@gitlab.com:ojehle/$repo
+fi
+
