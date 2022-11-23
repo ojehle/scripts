443bf61715fac4935608ea128eded2df9840c0f9
Oliver Jehle <ojehle@yahoo.com>
2022-11-23 07:43:10 +0100

    fix backup

--- synology/subversion_backup/subversion_backup.sh
+++ synology/subversion_backup/subversion_backup.sh
@@ -2,13 +2,14 @@
 cont_id=$(docker ps | grep subversion | awk '{print $1}')
 #echo $cont_id
 docker exec $cont_id /var/svn-backup/svn-backuper.sh
+rsync -axq /volume1/docker/subversion/backup/ /volume1/subversion_backup/
+rsync -axq /volume1/docker/subversion/dav_svn /volume1/subversion_backup/
+exit 0
 if [ ! -f /volume1/subversion_backup/last_backup.txt ]; then
   exit 0
 fi
-rsync -axq /volume1/docker/subversion/dav_svn /volume1/subversion_backup/
 files=$(awk -F/ '{print $(NF)}' /volume1/subversion_backup/last_backup.txt)
 for i in $files
 do
 	touch /volume1/subversion_backup/$i
-	chmod 750 /volume1/subversion_backup/$i
 done
