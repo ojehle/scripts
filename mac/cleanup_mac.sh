144233fe9ad10f7876b9a6a38cc72370460c8254
Oliver Jehle <ojehle@yahoo.com>
2022-12-12 13:15:51 +0100

    add cleanup mac

---
+++ mac/cleanup_mac.sh
@@ -0,0 +1,7 @@
+#rm -r ~/Library/Application\ Support/Microsoft/Teams
+rm -rf ~/Library/Application\ Support/SynologyDrive/log/daemon.log_*
+rm -rf ~/Library/Application\ Support/WebEx\ Folder
+rm -rf ~/Library/Application\ Support/Webex\ Meetings
+rm -rf ~/Library/Logs/Citrix\ Workspace/*.txt
+rm -rf ~/Library/Logs/AVGAntivirus/common[0-9].log
+
