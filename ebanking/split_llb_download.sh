7469d65f2c79465184f538bd21ff2b41ee6817e0
Oliver Jehle <ojehle@yahoo.com>
2023-01-09 10:25:45 +0100

    add file

---
+++ ebanking/split_llb_download.sh
@@ -0,0 +1,5 @@
+mv *LI0708800962705072001* $HOME/OneDrive\ -\ H35\ GmbH/OJ/Bank/llb/kontokorrent
+mv *LI2808800000007988708* $HOME/OneDrive\ -\ H35\ GmbH/OJ/Bank/llb/sparkonto
+mv *LI7708800962705072002* $HOME/OneDrive\ -\ H35\ GmbH/OJ/Bank/llb/baukonto/
+mv *.pdf $HOME/OneDrive\ -\ H35\ GmbH/OJ/Bank/llb/Diverses
+rm *.xml
